d18f97bce572678f1341c592a3e5c0bdcef66238
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-08-26 13:34:12 +0200

    Refactoring the code and adding some animations.

--- src/components/header.js
+++ src/components/header.js
@@ -6,28 +6,30 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import { Link } from "gatsby"
 
+import Logo from "../images/logoS.png";
+
 import headerStyles from "./header.module.scss";
 
 const Header = () => {
     return(
         <header className={headerStyles.header}>
-        <Link to="/" className={headerStyles.title}>Sebastian Zielonka</Link>
+        <Link to="/"><img src={Logo}  className={headerStyles.title} alt="Logo"></img></Link>
             <nav>
                 <ul className={headerStyles.navList}>
                     <li>
-                        <Link to="/" className={headerStyles.navItem}>Strona główna</Link>
+                        <Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Strona główna</Link>
                     </li>
                     <li>
-                        <Link to="/transformation" className={headerStyles.navItem}>Transformacje</Link>
+                        <Link to="/transformation" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Transformacje</Link>
                     </li>
                     <li>
-                        <Link to="/contact" className={headerStyles.navItem}>Kontakt</Link>
+                        <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Kontakt</Link>
                     </li>
                 </ul>
             </nav>
             <ul className={headerStyles.headerSocialMedia}>
-                <li><a href="https://www.instagram.com/sebastian__zielonka/?fbclid=IwAR3EAr29t_51d_TWlKWsP5hy7Wz_-7_8RVN_OutZw3f0FzcdRXeA33twVZk"><FontAwesomeIcon icon={faInstagram} size="2x"/></a></li>
-                <li><a href="https://www.facebook.com/Sebastian__Zielonka-105454837854619/"><FontAwesomeIcon icon={faFacebook} size="2x"/></a></li>
+                <li><a aria-label="link" href="https://www.instagram.com/sebastian__zielonka/?fbclid=IwAR3EAr29t_51d_TWlKWsP5hy7Wz_-7_8RVN_OutZw3f0FzcdRXeA33twVZk"><FontAwesomeIcon icon={faInstagram} size="2x"/></a></li>
+                <li><a aria-label="link" href="https://www.facebook.com/Sebastian__Zielonka-105454837854619/"><FontAwesomeIcon icon={faFacebook} size="2x"/></a></li>
             </ul>
         </header>
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,14 @@
 import React from "react"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import Header from "./header";
 import Footer from "./footer";
+
 import layoutStyles from "./layout.module.scss";
 import "../styles/index.scss";
+
+AOS.init({once: true})
 
 const Layout = (props) => {
     return(
