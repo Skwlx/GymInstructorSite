5c0690f4187259d3cf095b9b7c83037a76e09365
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-09-01 18:29:11 +0200

    Added Basic strapi integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000,
+        contentTypes: [`article`, `user`],
+      },
+    },
   ],
 }
--- src/pages/transformation.js
+++ src/pages/transformation.js
@@ -1,12 +1,47 @@
 import React from "react"
 import Layout from "../components/layout"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+import Marked from "marked"
+import SanitizeHtml from "sanitize-html";
 
-const Transformation = () =>{
+const Transformation = () => {
+  const data = useStaticQuery(graphql`
+  query MyQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          title
+          content
+          image {
+            childImageSharp {
+              fixed(width: 400) {
+                  base64
+                  width
+                  height
+                  src
+                  srcSet
+              }
+            }
+          }
+        }
+      }
+    }
+  }`)
+
   return (
   <div>
     <Layout>
       <div>
-        zmiany
+        {data.allStrapiArticle.edges.map(edge =>{
+          return (
+            <div>
+            <h1>{edge.node.title}</h1>
+            <p>{Marked(edge.node.content)}</p>
+            <Img fixed={edge.node.image.childImageSharp.fixed}></Img>
+            </div>
+          )
+        })}
       </div>
     </Layout>
   </div>
