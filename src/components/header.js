0b97596b85e23e5dd38fdc68dff7c81010574ba9
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-09-16 13:52:11 +0200

    Added index media query

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
     faInstagram,
@@ -14,10 +14,44 @@ import headerStyles from "../styles/componentsStyles/header.module.scss";
 import favIcon from "../images/fav2.ico";
 
 const Header = () => {
+
+    useEffect(() =>{
+
+        const addAnimation = (arrEl, animation) => {
+            [...burgerParts][arrEl].classList.add(animation);
+        };
+        const removeAnimation = (arrEl, animation) => {
+            [...burgerParts][arrEl].classList.remove(animation);
+        };
+
+        const burger = document.querySelector(`.${headerStyles.navContainerBurger}`);
+        const menu = document.querySelector(`.${headerStyles.navContainer}`);
+        let burgerParts = document.querySelectorAll(`.${headerStyles.navContainerBurgerLine}`)
+        let toggle = 0;
+        burger.addEventListener('click', () => {
+            if(toggle === 0){
+            toggle = 1;
+            addAnimation(0,headerStyles.disapear);
+            addAnimation(1,headerStyles.rotateToXA);
+            addAnimation(2,headerStyles.rotateToXB);
+            menu.classList.remove(headerStyles.hideMenu);
+            menu.classList.add(headerStyles.displayMenu);
+            }
+            else{
+                toggle = 0;
+                removeAnimation(0, headerStyles.disapear);
+                removeAnimation(1, headerStyles.rotateToXA);
+                removeAnimation(2, headerStyles.rotateToXB);
+                menu.classList.remove(headerStyles.displayMenu);
+                menu.classList.add(headerStyles.hideMenu);
+            }
+        })
+    })
     return(
         <header className={headerStyles.header}>
         <Helmet>
             <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width,initial-scale=1.0" /> 
             <title>Zielony trener</title>
             <meta name="description" content="Nazywam się Sebastian Zielonka, jestem trenerem Online." />
             <link rel="icon" type="image/png" href={favIcon}/>
@@ -29,6 +63,7 @@ const Header = () => {
             <li><a aria-label="link" href="https://www.facebook.com/Sebastian__Zielonka-105454837854619/"><FontAwesomeIcon icon={faFacebook} size="2x"/></a></li>
         </ul>
         </div>
+        <div className={headerStyles.navContainer}>
             <nav>
                 <ul className={headerStyles.navList}>
                     <li>
@@ -42,6 +77,12 @@ const Header = () => {
                     </li>
                 </ul>
             </nav>
+        </div>
+        <div className={headerStyles.navContainerBurger}>
+        <span className={headerStyles.navContainerBurgerLine}></span>
+        <span className={headerStyles.navContainerBurgerLine}></span>
+        <span className={headerStyles.navContainerBurgerLine}></span>
+        </div>
         </header>
 
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,28 +17,28 @@ const IndexPage = () =>{
         </div>
       </div>
       <div className={indexStyles.aboutMeContainer}>
-        <div className={indexStyles.aboutMeContent} data-aos="fade-up">
-        <h2 className={indexStyles.aboutMeContentTitle}>O mnie</h2>
-        <img className={indexStyles.aboutMePhoto} src={PhotoOfMe} alt="About Me"></img>
-        <div className={indexStyles.aboutMeContentText}>
+        <div className={indexStyles.aboutMeContent}>
+        <h2 className={indexStyles.aboutMeContentTitle} data-aos="fade-down">O mnie</h2>
+        <img className={indexStyles.aboutMePhoto} src={PhotoOfMe} alt="About Me" data-aos="fade-up"></img>
+        <div className={indexStyles.aboutMeContentText} data-aos="fade-up">
         <p>
-        Cześć, nazywam się Sebastian i jestem Trenerem Online. 
-        Studiuję dietetykę oraz prowadzę treningi personalne, 
+        Cześć, nazywam się Sebastian i jestem <span className={indexStyles.aboutMeContentTextImportant}>Trenerem Online.</span> 
+        <span className={indexStyles.aboutMeContentTextImportant}> Studiuję dietetykę oraz prowadzę treningi personalne</span>, 
         gdzie uczę z reguły osoby początkujące podstaw treningu siłowego.
         Część z was może mnie kojarzyć z social mediów,
         na których w prosty sposób staram się przekazywać wiedzę na temat dietetyki oraz treningu siłowego.
         </p>Tematem treningów oraz żywienia zacząłem interesować się już w 2012r.
         Od tamtego czasu stało się to moją pasją i postanowiłem rozwijać się w tym kierunku.
-        Prowadzę ludzi online już od ok. 4 lat, dzięki temu nabrałem już bardzo dużo doświadczenia.
-        Poza licznymi certyfikatami oraz zdobytym doświadczeniem posiadam dużą wiedzę,
+        <span className={indexStyles.aboutMeContentTextImportant}> Prowadzę ludzi online już od ok. {new Date().getFullYear() - 2016} lat,</span> dzięki temu nabrałem już bardzo dużo doświadczenia.
+        Poza licznymi certyfikatami oraz zdobytym doświadczeniem <span className={indexStyles.aboutMeContentTextImportant}>posiadam dużą wiedzę</span>,
         którą ciągle poszerzam, co przekłada się na lepszą współpracę z moimi podopiecznymi.
         <p><br/>
-        Wszystkie plany treningowe oraz diety dobieram indywidualnie do możliwości podopiecznych,
+        Wszystkie plany treningowe oraz diety dobieram <span className={indexStyles.aboutMeContentTextImportant}>indywidualnie</span> do możliwości podopiecznych,
         poza tym oferuję stały kontakt oraz naukę zdrowych nawyków żywieniowych,
-        co gwarantuje brak efektu jojo. Pomagam ludziom w osiągnięciu wymarzonej sylwetki,    
-        bez zbędnych restrykcji i pokazuję, że można jeść to na co ma się ochotę, to co się lubi, a przy tym mieć zadowalające efekty.
+        co gwarantuje brak efektu jojo.  <span className={indexStyles.aboutMeContentTextImportant}>Pomagam ludziom w osiągnięciu wymarzonej sylwetki</span>,    
+        bez zbędnych restrykcji i pokazuję, że można jeść to na co ma się ochotę, to co się lubi, a przy tym mieć zadowalające efekty. 
         Moim głównym celem jest uświadomienie ludzi, że można jeść zwyczajnie, spotykać się ze znajomymi, wychodzić na imprezy, 
-        a przy tym wszystkim cieszyć się dobrym zdrowiem, samopoczuciem i piękną sylwetką.
+        a przy tym wszystkim <span className={indexStyles.aboutMeContentTextImportant}>cieszyć się dobrym zdrowiem, samopoczuciem i piękną sylwetką.</span>
         </p>
         </div>
         </div>
