2499644320c37e8aa4892ea2b95075e27b6f1d61
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-09-21 18:30:17 +0200

    Added svg, and some css changes.

--- src/components/feed.js
+++ src/components/feed.js
@@ -6,7 +6,8 @@ import 'react-alice-carousel/lib/alice-carousel.css'
 import feedStyles from "../styles/componentsStyles/instaFeed.module.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-    faHeart
+    faHeart,
+    faComment
 } from '@fortawesome/free-solid-svg-icons'
 
 const Feed  = () => {
@@ -40,23 +41,28 @@ const Feed  = () => {
   }
   `)
 
+  // Setting amout of photos in one slide depends on screen width
   const responsive = {
     450: { items: 1 },
     800: {items: 2},
     1230: { items: 3 },
     1700: {items: 4}
   }
+  
+
 return (
   <div className={feedStyles.instaFeedContainer}>
-  <h3>INSTAGRAM</h3>
+  <h3 className={feedStyles.instaFeedContainerTitle}>INSTAGRAM</h3>
   <AliceCarousel 
   buttonsDisabled={true} 
   dotsDisabled={true} 
   autoPlay={true} 
   infinite={true} 
-  autoPlayInterval={1000} 
+  autoPlayInterval={2000} 
   responsive={responsive} 
-  autoHeight={true}>
+  autoHeight={true}
+  fadeOutAnimation={true}
+  >
 
   {instaData.allInstaNode.edges.map(edge => {
       return(
@@ -69,6 +75,10 @@ return (
         <p className={feedStyles.instaFeedContainerLikesAmount}>
         <FontAwesomeIcon className={feedStyles.instaFeedContainerPhotoLikes} icon={faHeart}/>
         {edge.node.likes}
+        </p>
+        <p className={feedStyles.instaFeedContainerCommentsAmount}>
+        <FontAwesomeIcon className={feedStyles.instaFeedContainerPhotoComments} icon={faComment}/>
+        {edge.node.comments}
         </p>
         </div>
       )
